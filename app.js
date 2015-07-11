52d5c194dfc04d1e6128aef155876e38a645243d
Kris MP <kris.mp7777@gmail.com>
2015-07-11 10:56:49 +0700

    implement bootstrap styling

--- app.js
+++ app.js
@@ -13,10 +13,12 @@ angular.module('flapperNews', [])
 				if (!$scope.title || $scope.title === '') { return; }
 				$scope.posts.push({
 					title: $scope.title,
+					link: $scope.link,
 					upvotes: 0
 				});
 
 				$scope.title = '';
+				$scope.link = '';
 			};
 			$scope.incrementUpvotes = function(post) {
 				post.upvotes += 1;
