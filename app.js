6afa8f8de4d2d2da13db6cca142e5705e9c6c70c
Kris MP <kris.mp7777@gmail.com>
2015-07-11 11:19:47 +0700

    create the post page

--- app.js
+++ app.js
@@ -53,5 +53,14 @@ angular.module('flapperNews', ['ui.router'])
 	.controller('PostsCtrl', ['$scope', '$stateParams', 'posts', function($scope, $stateParams, posts){
 		function($scope, $stateParams, posts) {
 			$scope.post = posts.posts[$stateParams.id];
+			$scope.addComment = function(){
+				if ($scope.body === '') { return; }
+				$scope.post.comments.push({
+					body: $scope.body,
+					author: 'user',
+					upvotes: 0
+				});
+				$scope.body = '';
+			};
 		}
 	}]);
